193956355886b882b9f753fb0a8f99a376ce46d2
dkaslovsky <dkaslovsky@gmail.com>
2020-12-11 14:20:37 -0700

    ensure appdir

--- main.go
+++ main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/dkaslovsky/TextNote/cmd"
 )
 
+const envAppDir = "TEXTNOTE_DIR"
+
 func main() {
-	err := cmd.Run()
+	err := ensureAppDir()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ensureAppDir() error {
+	appDir := os.Getenv(envAppDir)
+	if appDir == "" {
+		return fmt.Errorf("required environment variable [%s] is not set", envAppDir)
+	}
+	finfo, err := os.Stat(appDir)
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(appDir, 0755)
+		if err != nil {
+			return err
+		}
+		log.Printf("created directory [%s]", appDir)
+		return nil
+	}
+	if !finfo.IsDir() {
+		return fmt.Errorf("[%s=%s] must be a directory", envAppDir, appDir)
+	}
+	return nil
 }
