0b27b958704c085fd64856c4e0fa2730c3976777
dkaslovsky <dkaslovsky@gmail.com>
2020-12-13 14:31:29 -0700

    today cmd takes copy args

--- cmd/root.go
+++ cmd/root.go
@@ -1,25 +1,26 @@
 package cmd
 
 import (
-	"github.com/dkaslovsky/TextNote/cmd/open"
+	"github.com/dkaslovsky/TextNote/cmd/today"
 	"github.com/spf13/cobra"
 )
 
-// Run executes the CLI interface
+// Run executes the CLI
 func Run() error {
 	cmd := &cobra.Command{
-		Use:   "textnote",
-		Short: "open today's note",
-		Long:  "open a text based note template for today",
+		Use:           "textnote",
+		Short:         "open today's note",
+		Long:          "open a text based note template for today",
+		SilenceUsage:  true,
+		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// run the today command as the default
-			return open.CreateTodayCmd().Execute()
+			return today.CreateTodayCmd().Execute()
 		},
 	}
 
 	cmd.AddCommand(
-		open.CreateTodayCmd(),
-		open.CreateTomorrowCmd(),
+		today.CreateTodayCmd(),
 	)
 
 	return cmd.Execute()
---
+++ cmd/today/today.go
@@ -0,0 +1,96 @@
+package today
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dkaslovsky/TextNote/pkg/config"
+	"github.com/dkaslovsky/TextNote/pkg/file"
+	"github.com/dkaslovsky/TextNote/pkg/template"
+	"github.com/spf13/cobra"
+)
+
+// CommandOptions are command options
+type CommandOptions struct {
+	Copy   []string
+	Delete bool
+}
+
+// CreateTodayCmd creates the package's subcommand
+func CreateTodayCmd() *cobra.Command {
+	cmdOpts := CommandOptions{}
+	cmd := &cobra.Command{
+		Use:   "today",
+		Short: "open today's note",
+		Long:  "open a text based note template for today",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts, err := config.LoadOrCreate()
+			if err != nil {
+				return err
+			}
+
+			shouldCopy := len(cmdOpts.Copy) > 0
+
+			date := time.Now()
+			t := template.NewTemplate(opts)
+			t.SetDate(date)
+
+			if !shouldCopy {
+				err = file.CreateIfNotExists(t)
+				if err != nil {
+					return err
+				}
+				return file.OpenInEditor(t.GetFilePath(), t.GetFirstSectionFirstLine())
+			}
+
+			if file.Exists(t) {
+				err = file.Read(t)
+				if err != nil {
+					return err
+				}
+			}
+
+			srcDate := date.Add(-24 * time.Hour)
+			src := template.NewTemplate(opts)
+			src.SetDate(srcDate)
+			err = file.Read(src)
+			if err != nil {
+				return err
+			}
+
+			copyOp := t.CopySectionContents
+			if cmdOpts.Delete {
+				copyOp = t.MoveSectionContents
+			}
+
+			for _, sectionName := range cmdOpts.Copy {
+				err = copyOp(src, sectionName)
+				if err != nil {
+					return err
+				}
+			}
+			if cmdOpts.Delete {
+				err = file.Write(src)
+				if err != nil {
+					return err
+				}
+			}
+			fmt.Println("t:")
+			t.Write(os.Stdout)
+			err = file.Write(t)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+	attachOpts(cmd, &cmdOpts)
+	return cmd
+}
+
+func attachOpts(cmd *cobra.Command, cmdOpts *CommandOptions) {
+	flags := cmd.Flags()
+	flags.StringSliceVarP(&cmdOpts.Copy, "copy", "c", []string{}, "section names to copy")
+	flags.BoolVarP(&cmdOpts.Delete, "delete", "d", false, "delete previous day's section after copy (no-op without copy")
+}
