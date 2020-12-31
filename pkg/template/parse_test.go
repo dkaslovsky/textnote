180e821fda4ee9fee878b73bbab142d28ff64e14
dkaslovsky <dkaslovsky@gmail.com>
2020-12-31 15:08:46 -0700

    parseSectionContents tests

--- pkg/template/parse_test.go
+++ pkg/template/parse_test.go
@@ -1,8 +1,10 @@
 package template
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/dkaslovsky/TextNote/pkg/config"
 	"github.com/stretchr/testify/require"
@@ -10,14 +12,14 @@ import (
 
 var testOpts = config.Opts{
 	Header: config.HeaderOpts{
-		Prefix:           "--",
-		Suffix:           "--",
+		Prefix:           "-^-",
+		Suffix:           "-v-",
 		TrailingNewlines: 1,
 		TimeFormat:       "[Mon] 02 Jan 2006",
 	},
 	Section: config.SectionOpts{
-		Prefix:           "___",
-		Suffix:           "___",
+		Prefix:           "_p_",
+		Suffix:           "_q_",
 		TrailingNewlines: 3,
 		Names: []string{
 			"TestSection1",
@@ -29,8 +31,8 @@ var testOpts = config.Opts{
 		TimeFormat: "2006-01-02",
 	},
 	Archive: config.ArchiveOpts{
-		HeaderPrefix:             "ARCHIVE ",
-		HeaderSuffix:             " ARCHIVE",
+		HeaderPrefix:             "ARCHIVEPREFIX ",
+		HeaderSuffix:             " ARCHIVESUFFIX",
 		SectionContentPrefix:     "[",
 		SectionContentSuffix:     "]",
 		SectionContentTimeFormat: "2006-01-02",
@@ -38,15 +40,44 @@ var testOpts = config.Opts{
 	},
 }
 
+var testDate = time.Date(2020, 12, 20, 1, 1, 1, 1, time.UTC)
+
+func makeTestItemHeader(date time.Time, opts config.Opts) string {
+	return fmt.Sprintf("%s%s%s",
+		opts.Archive.HeaderPrefix,
+		date.Format(opts.Archive.SectionContentTimeFormat),
+		opts.Archive.HeaderSuffix,
+	)
+}
+
 func TestParseSectionContents(t *testing.T) {
 	type testCase struct {
-		body     string
+		lines    []string
 		expected []contentItem
 	}
 
 	tests := map[string]testCase{
-		"non-archive": {
-			body: "\n\nhello\n  world\n\n",
+		"empty lines": {
+			lines:    []string{},
+			expected: []contentItem{},
+		},
+		"single empty string line": {
+			lines: []string{""},
+			expected: []contentItem{
+				{},
+			},
+		},
+		"lines with no header": {
+			lines: strings.Split("hello\n  world", "\n"),
+			expected: []contentItem{
+				{
+					header: "",
+					text:   "hello\n  world",
+				},
+			},
+		},
+		"lines with no header with newline at start and end": {
+			lines: strings.Split("\n\nhello\n  world\n\n", "\n"),
 			expected: []contentItem{
 				{
 					header: "",
@@ -54,12 +85,106 @@ func TestParseSectionContents(t *testing.T) {
 				},
 			},
 		},
+		"lines with single header": {
+			lines: strings.Split(
+				fmt.Sprintf("%s\nhello\n  world", makeTestItemHeader(testDate, testOpts)),
+				"\n",
+			),
+			expected: []contentItem{
+				{
+					header: makeTestItemHeader(testDate, testOpts),
+					text:   "hello\n  world",
+				},
+			},
+		},
+		"lines with single header with newline at start and end": {
+			lines: strings.Split(
+				fmt.Sprintf("\n%s\n\nhello\n  world\n", makeTestItemHeader(testDate, testOpts)),
+				"\n",
+			),
+			expected: []contentItem{
+				{},
+				{
+					header: makeTestItemHeader(testDate, testOpts),
+					text:   "\nhello\n  world\n",
+				},
+			},
+		},
+		"lines with multiple headers": {
+			lines: strings.Split(
+				fmt.Sprintf("%s\nhello\n  world\n%s\nhello2\n  world2",
+					makeTestItemHeader(testDate, testOpts),
+					makeTestItemHeader(testDate.Add(24*time.Hour), testOpts),
+				),
+				"\n",
+			),
+			expected: []contentItem{
+				{
+					header: makeTestItemHeader(testDate, testOpts),
+					text:   "hello\n  world",
+				},
+				{
+					header: makeTestItemHeader(testDate.Add(24*time.Hour), testOpts),
+					text:   "hello2\n  world2",
+				},
+			},
+		},
+		"lines with multiple headers with newline at start and end": {
+			lines: strings.Split(
+				fmt.Sprintf("\n%s\nhello\n  world\n\n%s\nhello2\n  world2\n",
+					makeTestItemHeader(testDate, testOpts),
+					makeTestItemHeader(testDate.Add(24*time.Hour), testOpts),
+				),
+				"\n",
+			),
+			expected: []contentItem{
+				{},
+				{
+					header: makeTestItemHeader(testDate, testOpts),
+					text:   "hello\n  world\n",
+				},
+				{
+					header: makeTestItemHeader(testDate.Add(24*time.Hour), testOpts),
+					text:   "hello2\n  world2\n",
+				},
+			},
+		},
+		"header with no text": {
+			lines: strings.Split(
+				makeTestItemHeader(testDate, testOpts),
+				"\n",
+			),
+			expected: []contentItem{
+				{
+					header: makeTestItemHeader(testDate, testOpts),
+					text:   "",
+				},
+			},
+		},
+		"multiple headers with no text": {
+			lines: strings.Split(
+				fmt.Sprintf("%s\n%s",
+					makeTestItemHeader(testDate, testOpts),
+					makeTestItemHeader(testDate.Add(24*time.Hour), testOpts),
+				),
+				"\n",
+			),
+			expected: []contentItem{
+				{
+					header: makeTestItemHeader(testDate, testOpts),
+					text:   "",
+				},
+				{
+					header: makeTestItemHeader(testDate.Add(24*time.Hour), testOpts),
+					text:   "",
+				},
+			},
+		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			lines := strings.Split(test.body, "\n")
-			contents := parseSectionContents(lines, testOpts.Section.Prefix, testOpts.Section.Suffix, testOpts.File.TimeFormat)
+			contents := parseSectionContents(test.lines, testOpts.Archive.HeaderPrefix, testOpts.Archive.HeaderSuffix, testOpts.File.TimeFormat)
 			require.Equal(t, test.expected, contents)
 		})
 	}
