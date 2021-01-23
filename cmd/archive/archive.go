79825a6d93981ab86841dad114151706a910761d
dkaslovsky <dkaslovsky@gmail.com>
2021-01-23 14:37:40 -0700

    archive run func

--- cmd/archive/archive.go
+++ cmd/archive/archive.go
@@ -14,8 +14,8 @@ import (
 )
 
 type commandOptions struct {
-	Delete  bool
-	NoWrite bool
+	delete  bool
+	noWrite bool
 }
 
 // CreateArchiveCmd creates the today subcommand
@@ -30,52 +30,7 @@ func CreateArchiveCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
-
-			archiver := archive.NewArchiver(opts, file.NewReadWriter(), time.Now())
-
-			files, err := ioutil.ReadDir(config.AppDir)
-			if err != nil {
-				return err
-			}
-
-			// add files to archiver
-			archived := []string{}
-			for _, f := range files {
-				if !archive.ShouldArchive(f, opts.Archive.FilePrefix) {
-					log.Printf("file [%s] will not be archived", f.Name())
-					continue
-				}
-				err := archiver.Add(f)
-				if err != nil {
-					log.Printf("file [%s] will not be archived: %s", f.Name(), err)
-					continue
-				}
-				archived = append(archived, f.Name())
-			}
-
-			// write archive files
-			if !cmdOpts.NoWrite {
-				err = archiver.Write()
-				if err != nil {
-					return err
-				}
-			}
-
-			// return if not deleting archived files
-			if !cmdOpts.Delete {
-				return nil
-			}
-
-			// delete individual archived files
-			for _, f := range archived {
-				err = os.Remove(filepath.Join(config.AppDir, f))
-				if err != nil {
-					log.Printf("unable to remove file [%s]: %s", f, err)
-					continue
-				}
-			}
-
-			return nil
+			return run(opts, cmdOpts)
 		},
 	}
 	attachOpts(cmd, &cmdOpts)
@@ -84,6 +39,54 @@ func CreateArchiveCmd() *cobra.Command {
 
 func attachOpts(cmd *cobra.Command, cmdOpts *commandOptions) {
 	flags := cmd.Flags()
-	flags.BoolVarP(&cmdOpts.Delete, "delete", "d", false, "delete individual files after archiving")
-	flags.BoolVarP(&cmdOpts.NoWrite, "nowrite", "n", false, "disable writing archive file (helpful for deleting previously archived files)")
+	flags.BoolVarP(&cmdOpts.delete, "delete", "x", false, "delete individual files after archiving")
+	flags.BoolVarP(&cmdOpts.noWrite, "nowrite", "n", false, "disable writing archive file (helpful for deleting previously archived files)")
+}
+
+func run(templateOpts config.Opts, cmdOpts commandOptions) error {
+	archiver := archive.NewArchiver(templateOpts, file.NewReadWriter(), time.Now())
+
+	files, err := ioutil.ReadDir(config.AppDir)
+	if err != nil {
+		return err
+	}
+
+	// add files to archiver
+	archived := []string{}
+	for _, f := range files {
+		if !archive.ShouldArchive(f, templateOpts.Archive.FilePrefix) {
+			log.Printf("file [%s] will not be archived", f.Name())
+			continue
+		}
+		err := archiver.Add(f)
+		if err != nil {
+			log.Printf("file [%s] will not be archived: %s", f.Name(), err)
+			continue
+		}
+		archived = append(archived, f.Name())
+	}
+
+	// write archive files
+	if !cmdOpts.noWrite {
+		err = archiver.Write()
+		if err != nil {
+			return err
+		}
+	}
+
+	// return if not deleting archived files
+	if !cmdOpts.delete {
+		return nil
+	}
+
+	// delete individual archived files
+	for _, f := range archived {
+		err = os.Remove(filepath.Join(config.AppDir, f))
+		if err != nil {
+			log.Printf("unable to remove file [%s]: %s", f, err)
+			continue
+		}
+	}
+
+	return nil
 }
