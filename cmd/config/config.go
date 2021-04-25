549c686ac49387aa560f8f4ba6d54e4589de27a6
dkaslovsky <dkaslovsky@gmail.com>
2021-04-25 13:48:17 -0600

    config cmd flag to show active configuration

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -9,10 +9,13 @@ import (
 	"github.com/dkaslovsky/textnote/pkg/config"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
 type commandOptions struct {
-	path bool
+	path   bool
+	active bool
+	file   bool
 }
 
 // CreateConfigCmd creates the config subcommand
@@ -26,24 +29,16 @@ func CreateConfigCmd() *cobra.Command {
 			configPath := config.GetConfigFilePath()
 
 			if cmdOpts.path {
-				log.Printf("configuration file path: [%s]\n", configPath)
+				log.Printf("configuration file path: [%s]", configPath)
 				return nil
 			}
 
-			_, err := os.Stat(configPath)
-			if os.IsNotExist(err) {
-				return fmt.Errorf("cannot find configuration file [%s]", configPath)
+			if cmdOpts.active {
+				return displayActiveConfig()
 			}
-			f, err := os.Open(configPath)
-			if err != nil {
-				return errors.Wrapf(err, "unable to open configuration file [%s]", configPath)
-			}
-			c, err := io.ReadAll(f)
-			if err != nil {
-				return errors.Wrapf(err, "unable to read configuration file [%s]", configPath)
-			}
-			log.Printf("%s", c)
-			return nil
+
+			// default
+			return displayConfigFile(configPath)
 		},
 	}
 	attachOpts(cmd, &cmdOpts)
@@ -52,5 +47,39 @@ func CreateConfigCmd() *cobra.Command {
 
 func attachOpts(cmd *cobra.Command, cmdOpts *commandOptions) {
 	flags := cmd.Flags()
-	flags.BoolVarP(&cmdOpts.path, "path", "p", false, "print path to configuration file")
+	flags.BoolVarP(&cmdOpts.path, "path", "p", false, "display path to configuration file")
+	flags.BoolVarP(&cmdOpts.active, "active", "a", false, "display configuration the application actively uses (includes environment variable configuration)")
+	flags.BoolVarP(&cmdOpts.file, "file", "f", false, "display contents of configuration file (default)")
+}
+
+func displayConfigFile(configPath string) error {
+	_, err := os.Stat(configPath)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("cannot find configuration file [%s]", configPath)
+	}
+	f, err := os.Open(configPath)
+	if err != nil {
+		return errors.Wrapf(err, "unable to open configuration file [%s]", configPath)
+	}
+	c, err := io.ReadAll(f)
+	if err != nil {
+		return errors.Wrapf(err, "unable to read configuration file [%s]", configPath)
+	}
+	log.Print(string(c))
+	return nil
+}
+
+func displayActiveConfig() error {
+	opts, err := config.Load()
+	if err != nil {
+		return err
+	}
+
+	yml, err := yaml.Marshal(opts)
+	if err != nil {
+		return err
+	}
+
+	log.Print(string(yml))
+	return nil
 }
