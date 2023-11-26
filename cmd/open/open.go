96538f87cb5b27e5b9ccbfec9c69e43f8ba40d47
dkaslovsky <dkaslovsky@gmail.com>
2023-11-26 09:34:11 -0700

    handle malformed copy date before running open cmd

--- cmd/open/open.go
+++ cmd/open/open.go
@@ -146,6 +146,9 @@ func setCopyDateOpt(cmdOpts *commandOptions, templateOpts config.Opts, getFiles 
 	}
 
 	if cmdOpts.copyDate != "" {
+		if _, err := time.Parse(templateOpts.Cli.TimeFormat, cmdOpts.copyDate); err != nil {
+			return false, errors.Wrapf(err, "cannot copy note from malformed date [%s]", cmdOpts.copyDate)
+		}
 		return false, nil
 	}
 	if cmdOpts.copyDaysBack != 0 {
